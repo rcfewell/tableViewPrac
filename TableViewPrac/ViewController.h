2bc7857cf07a21285ef5ed200c8c5fb09f7b1ec9
Riley Fewell <rileyfewell@Rileys-MacBook-Pro.local>
2016-02-17 07:44:47 -0800

    Initial Commit

---
+++ TableViewPrac/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewPrac
+//
+//  Created by Riley Fewell on 2/17/16.
+//  Copyright (c) 2016 Riley Fewell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TableViewPrac/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TableViewPrac
+//
+//  Created by Riley Fewell on 2/17/16.
+//  Copyright (c) 2016 Riley Fewell. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
