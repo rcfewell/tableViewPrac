6b44d6155b6d5774da40b267079196ea851ce317
Riley Fewell <rileyfewell@Rileys-MacBook-Pro.local>
2016-02-17 09:44:09 -0800

    created a simple table view

--- TableViewPrac/ViewController.m
+++ TableViewPrac/ViewController.m
@@ -9,17 +9,55 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    NSArray * recipes;
+}
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    recipes = [NSArray arrayWithObjects:@"Eggs Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwhich", @"Creme Brulee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
 }
 
-- (void)didReceiveMemoryWarning {
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [recipes count];
+}
+
+- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if( cell == nil )
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+        
+//        CALayer * cellImageLayer = cell.imageView.layer;
+//        [cellImageLayer setCornerRadius:cellImageLayer.frame.size.height / 2];
+//        [cellImageLayer setMasksToBounds:YES];
+    }
+    
+    cell.textLabel.text = [recipes objectAtIndex:indexPath.row];
+    
+    [cell.imageView.layer setCornerRadius:cell.frame.size.height / 2];
+    [cell.imageView.layer setMasksToBounds:YES];
+//    [cell.imageView.layer setBorderWidth:2.0];
+//    [cell.imageView.layer setBorderColor:[UIColor blackColor].CGColor];
+    cell.imageView.image = [UIImage imageNamed:@"creme_brelee.jpg"];
+    return cell;
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
